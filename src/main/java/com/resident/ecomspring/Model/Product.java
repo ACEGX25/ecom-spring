cefc2d00d0d3fbc442202ef4a246a9593f7413f4
SILLYCAT <143728245+ACEGX25@users.noreply.github.com>
2025-01-22 08:27:06 +0530

    didnt run, only set up the model, controller and service folders. Initiated the product model

---
+++ src/main/java/com/resident/ecomspring/Model/Product.java
@@ -0,0 +1,29 @@
+package com.resident.ecomspring.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Component
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Product {
+    @Id
+    private int prodId;
+    private String prodName;
+    private String prodDesc;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releasedate;
+    private boolean available;
+    private int quantity;
+}
